39ce791724960d3610439f5e83b2c2db5d3ca614
castaneai <castaneai@castaneai.net>
2021-02-19 04:23:46 +0900

    fix entrypoint

--- services/gaming/game/entrypoint.sh
+++ services/gaming/game/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "Waiting for X socket"
-until [[ -S /tmp/.X11-unix/X${DISPLAY/:/} ]]; do sleep 1; done
-echo "X socket is ready"
-
 # Run xvfb to avoid error "Failed to query current display settings..."
 Xvfb "${DISPLAY}" -screen 0 1280x960x24 &
 x11vnc -display WAIT"${DISPLAY}" -shared -forever -passwd 1234 -q &
--- services/gaming/gameagent/entrypoint.sh
+++ services/gaming/gameagent/entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-set -ex
-echo "Waiting for X socket"
-until [[ -S /tmp/.X11-unix/X${DISPLAY/:/} ]]; do sleep 1; done
-echo "X socket is ready"
 "$@"
